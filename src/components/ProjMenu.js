d31ffd31e6ab2e983a62694db5b71b96e5bd1b9a
Andrew Dickinson <andrew.dickinson@tee2.org>
2017-12-04 18:00:37 -0800

    fixd styling for mobile vies and projMenu

--- src/components/ProjMenu.js
+++ src/components/ProjMenu.js
@@ -4,9 +4,9 @@ export default class ProjMenu extends Component{
   render(){
     return(
       <div id="projMenu">
-        <NavLink exact to='/projects/mkc'> Military Kids Connect </NavLink>
-        <NavLink to ='/projects/krandrew'> Krystaal & Andrew's Wedding Site </NavLink>
- 				<NavLink to ='/projects/to-the-9s'> To the 9's </NavLink>
+        <NavLink exact to='/projects/mkc' activeClassName="projActive"> Military Kids Connect </NavLink>
+        <NavLink exact to ='/projects/krandrew' activeClassName="projActive"> Krystaal & Andrew's Wedding Site </NavLink>
+ 				<NavLink exact to ='/projects/to-the-9s' activeClassName="projActive"> To the 9's </NavLink>
       </div>
     )
   }
